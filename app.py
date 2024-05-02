0ff50098155bdd7ea980a1716a5cae8ca0890ebc
Ulysse Renzetti <ulysse.renzetti@outlook.fr>
2024-05-02 17:55:58 +0200

    add filesq

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import render_template,Flask
+
+app: Flask = Flask(__name__)
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+
