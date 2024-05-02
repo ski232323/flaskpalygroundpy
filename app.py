7915b0ee57a5db0f619ff5e8db8519791bf78dce
Ulysse Renzetti <ulysse.renzetti@outlook.fr>
2024-05-02 18:24:21 +0200

    add files and favicon

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import render_template, Flask
 app: Flask = Flask(__name__)
 
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+@app.route('/')
+@app.route('/users/<user>')
+def hello(user=None):
+    return render_template('index.html', name=user)
