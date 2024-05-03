35fe2d5c47f5ac49abb916fcc88df1df92970200
Ulysse Renzetti <ulysse.renzetti@outlook.fr>
2024-05-03 14:50:42 +0200

    add files and favicon

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import render_template,Flask
-
-app: Flask = Flask(__name__)
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+from flask import Flask, request, render_template
 
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    # Retrieve the user's IP address
+    user_ip = request.remote_addr
+    return render_template('hello.html', ip=user_ip)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
