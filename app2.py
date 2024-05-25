fc4248bc078328561d7d1ca4facb5a4800dbddb6
ski232323 <152718343+ski232323@users.noreply.github.com>
2024-05-25 12:52:56 +0200

    Delete app2.py

--- app2.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template , Flask
-
-app: Flask = Flask(__name__)
-
-
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
