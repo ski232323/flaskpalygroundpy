c8218663486e93eb52ffb25a6f3cdc3ba2c8c7a4
Ulysse Renzetti <ulysse.renzetti@outlook.fr>
2024-05-25 12:56:34 +0200

    add files

--- app.py
+++ app.py
@@ -1,14 +1,60 @@
-from flask import Flask, request, render_template
-
+from flask import Flask, render_template, redirect, url_for, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ski'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 
-@app.route('/')
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=25)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+
+class User(UserMixin):
+    def __init__(self, id2):
+        self.id = id2
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        # Simple authentication logic (replace with your own logic)
+        if username == 'admin' and password == 'password':
+            user = User(id2=1)
+            login_user(user)
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid username or password')
+    return render_template('login.html', form=form)
+
+
+@app.route('/index')
+@login_required
 def index():
-    # Retrieve the user's IP address
-    user_ip = request.remote_addr
-    return render_template('hello.html', ip=user_ip)
+    return render_template('index.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
