ce1d8b44c27210f625b1947bea79ea1f4ed88b69
ulysse.renzetti <ulysse.renzetti@outlook.fr>
2024-06-16 04:28:03 -0700

    hello

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from tinydb import TinyDB, Query
+from script import add
 import json
 from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 
 db = TinyDB('db.json')
 
+add(1,2)
 
